94f17ca2d1280bd4d2d0390d50c8da60b715f688
alexh01 <aheifler@stevens.edu>
2020-05-04 16:42:01 -0400

    fixed some types

--- Program files/functions.h
+++ Program files/functions.h
@@ -66,29 +66,24 @@ bool deactivate_CC() {
 	set_speed = 0;
 	return !state;
 }
-double change_CC_set_speed(double usr_set_speed) {//TODO: touch up the write statements
+double change_CC_set_speed(double usr_set_speed) {
 	//NOTE: returns -1 on a failure to initiate cruising and the set speed on Success
-	if(state) {
-		fprintf(stderr, "ALERT: You're already Cruising\n");//alternatively call deactivate and return 0
-		//write(LOG_FILENO, "User attempted to initiate activated CC\n", 41);
-		return -1;
-	}
-	if(current_speed < 20) {
-		fprintf(stderr, "ALERT: Driver cannot initiate cruise control below 20 mph\n");
-		//write(LOG_FILENO, "User attempted to initiate CC outside the range of 20mph to 110mph\n", 68);
+	if(usr_set_speed < 20) {
+		fprintf(stderr, "ALERT: Driver cannot set cruise control speed below 20 mph\n");
+		//write(LOG_FILENO, "User attempted to initiate CC outside the range of 20mph to 110mph\n", 69);
 		//TODO add in the current speed to the log
 		return -1;
-	} else if(current_speed > 110) {
-		fprintf(stderr, "ALERT: Driver cannot initiate cruise control above 110 mph\n");
-		//write(LOG_FILENO, "User attempted to initiate CC outside the range of 20mph to 110mph\n", 68);
+	} else if(usr_set_speed > 110) {
+		fprintf(stderr, "ALERT: Driver cannot set cruise control speed above 110 mph\n");
+		//write(LOG_FILENO, "User attempted to set CC speed outside the range of 20mph to 110mph\n", 69);
 		//TODO add in the current speed to the log
 		return -1;
 	}
-	printf("Begin Cruising\n");
+	printf("Cruising at %ld mph\n", usr_set_speed);
 	//write(LOG_FILENO, "User attempted to deactivate uninitiated or idle CC\n", 53);
 	state = true;
-	set_speed = current_speed;
-	return current_speed;
+	set_speed = usr_set_speed;
+	return usr_set_speed;
 }
 
 #endif
